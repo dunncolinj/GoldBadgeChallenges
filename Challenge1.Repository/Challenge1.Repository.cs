bf535e1b79acd81f024a06fbd816b717e0d50c07
Colin Dunn <dunncolinj@gmail.com>
2021-10-21 19:36:19 -0400

    Add project files.

---
+++ Challenge1.Repository/Challenge1.Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1.Repository
+{
+    public class MenuItem
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Menu
+    {
+        public List<MenuItem> _menu;
+
+    }
+    public class MenuRepository
+    {
+        Menu _menu = new Menu();
+            
+        public void AddMenuItem(MenuItem itemToAdd)
+        {
+            
+        }
+
+
+        public void DeleteMenuItem(MenuItem itemToRemove)
+        {
+
+        }
+
+        public void ShowMenuItems()
+        {
+            foreach(MenuItem item in _menu)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Challenge1.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge1.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge1.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9c650c0-bc87-427b-8830-373c76d0c236")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
